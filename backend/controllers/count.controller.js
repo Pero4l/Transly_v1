2ddcb263bb54abe660e23b6afb25726020ebedc5
pero4l <lilpero03@gmail.com>
2026-04-27 14:34:36 +0100

    add counts all users

--- backend/controllers/count.controller.js
+++ backend/controllers/count.controller.js
@@ -1,1 +1,19 @@
-const {Users} = require("../models")
+const {Users, Shipment} = require("../models")
+
+const countAll = async (req, res) => {
+    
+    try {
+        const allUsers = await Users.count()
+        const allShipment = await Shipment.count();
+        res.status(200).json({ 
+            success: true, 
+            allUsers, 
+            allShipment,
+            message: "Gotten all Count successfully"
+         });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+module.exports = {countAll};
